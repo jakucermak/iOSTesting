0a74ce16fca1c5b0ae7d4a92e09ea9f26afa8c6a
Jakub <jakucermak@gmail.com>
2023-02-15 14:39:54 +0100

    created Client layer for automated testing

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import wda
 
-
 class IOSCLient():
-    
-    
+    def __init__(self, port):
+          self.port = port
+
+    def start_client(self):
+
+        client = wda.Client(f"http://localhost:{self.port}")
+        return client
--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-
 import uvicorn
-from fastapi import FastAPI, BackgroundTasks 
+from fastapi import FastAPI, BackgroundTasks
 
 from utils.utils import available_devices, start_wda_service
 
+from client import IOSCLient
 app = FastAPI()
+
+devices = []
+clients = {}
 
 
 @app.get("/")
 async def root():
-    return {"available_devices": available_devices()}
+    global devices
+    devices = available_devices()
+    return {"available_devices": devices}
 
 
 @app.post("/start/")
-async def startDevice(udid: str, port: int,background_task: BackgroundTasks):
-    background_task.add_task(start_wda_service, udid,port)
-    return {"message" : "staring wdaservice in background" }
+async def start_device(udid: str, port: int, background_task: BackgroundTasks):
+    background_task.add_task(start_wda_service, udid, port)
+    global clients
+
+    client = IOSCLient(port).start_client()
+
+    clients[udid] = client
+
+    return {"message": client.status()}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
